9354eddf1bfb6893db4e2e30f6620fc0ed2c7442
altrhombus <altrhombus@outlook.com>
2024-03-18 18:01:01 -0500

    try a new source for ltsc information

--- scripts/ms/mswin/lifecycle-client-ltsc.ps1
+++ scripts/ms/mswin/lifecycle-client-ltsc.ps1
@@ -1,30 +1,32 @@
-$msdata = @("https://learn.microsoft.com/en-us/lifecycle/products/windows-10-2016-ltsb", "https://learn.microsoft.com/en-us/lifecycle/products/windows-10-enterprise-ltsc-2019")
+$msdata = @("https://learn.microsoft.com/en-us/windows/release-health/release-information")
 $d4nData = Invoke-WebRequest "https://raw.githubusercontent.com/altrhombus/ReportSource/main/content/ms/mswin/lifecycle-client-ltsc.json" | Select-Object -ExpandProperty Content | ConvertFrom-Json
 
 $releaseList = New-Object System.Collections.ArrayList
 
 foreach ($sourceURL in $msdata) {
     $source = Invoke-WebRequest $sourceURL -UseBasicParsing
-    $allReleases = [RegEx]::New(('<td>(.*?)<\/td>\s*<td align="right">\s*<local-time timezone="America\/Los_Angeles" format="date" datetime="(.*?)">(.*?)<\/local-time>\s*<\/td>\s*<td align="right">\s*<local-time timezone="America\/Los_Angeles" format="date" datetime="(.*?)">(.*?)<\/local-time>\s*<\/td>\s*<td align="right">\s*<local-time timezone="America\/Los_Angeles" format="date" datetime="(.*?)">(.*?)<\/local-time>')).Matches($source.RawContent)
+    $allReleases = [RegEx]::New(('<tr><td>(.*?)</td>\s*<td align="left">(.*?)</td>\s*<td>(.*?)</td>\s*<td>(.*?)</td>\s*<td>(.*?)</td>\s*<td>(.*?)</td>\s*<td>(.*?)</td>\s*</tr>')).Matches($source.RawContent)
     $allReleases.ForEach{
-        $releaseList.add(
-            [PSCustomObject]@{
-                Version = $_.Groups[1].value
-                SKU = "LTSC"
-                StartDate = $(Get-Date $_.Groups[2].Value -Format "yyyy-MM-dd")
-                MainstreamEndDate = $(Get-Date $_.Groups[4].Value -Format "yyyy-MM-dd")
-                ExtendedEndDate = $(Get-Date $_.Groups[6].Value -Format "yyyy-MM-dd")
-            }
-        ) | Out-Null
+            $releaseList.add(
+                [PSCustomObject]@{
+                    Version = $_.Groups[1].Value
+                    ServicingOption = $_.Groups[2].Value
+                    Build = $_.Groups[5].Value.Split(".")[0]
+                    StartDate = $_.Groups[3].Value
+                    MainstreamEndDate = $_.Groups[6].Value
+                    ExtendedEndDate = $_.Groups[7].Value
+                }
+            ) | Out-Null
+        
     }
 }
 
-$releaseList = $releaseList | Sort-Object Version | Select-Object Version,SKU,StartDate,MainstreamEndDate,ExtendedEndDate -Unique
+$releaseList = $releaseList | Sort-Object Version | Select-Object Version,ServicingOption,Build,StartDate,MainstreamEndDate,ExtendedEndDate -Unique
 
 $outputData = [PSCustomObject]@{
     "DataForNerds"=[PSCustomObject]@{
         "LastUpdatedUTC" = (Get-Date).ToUniversalTime()
-        "SourceList" = @("https://learn.microsoft.com/en-us/lifecycle/products/windows-10-2016-ltsb", "https://learn.microsoft.com/en-us/lifecycle/products/windows-10-enterprise-ltsc-2019")
+        "SourceList" = @("https://learn.microsoft.com/en-us/windows/release-health/release-information")
     }
     "Data" = $releaseList
 }
